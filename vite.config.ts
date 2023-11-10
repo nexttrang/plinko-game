85b21ace2db63c95a6385c5ea579759963ed575c
Trung Trang <nexttrang@gmail.com>
2023-11-10 15:28:47 +0700

    Update Base URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsConfigPaths()]
+  plugins: [react(), tsConfigPaths()],
+  base: "http://plinko-game.github.io",
 })
